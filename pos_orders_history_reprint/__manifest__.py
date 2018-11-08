b9e74598e9fb007038c376caa93d162bea45a750
GabbasovDinar <gabbasov@it-projects.info>
2018-11-08 17:36:29 +0500

    test

--- pos_orders_history_reprint/__manifest__.py
+++ pos_orders_history_reprint/__manifest__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Losev Artyom
 # Copyright 2018 Dinar Gabbasov <https://it-projects.info/team/GabbasovDinar>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
